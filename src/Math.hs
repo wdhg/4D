3ece798114f81ed45966fc5b3a55667e1face66c
thg19 <thg19@ic.ac.uk>
2020-06-30 15:37:29 +0100

    add Nums typeclass

--- src/Math.hs
+++ src/Math.hs
@@ -1,5 +1,13 @@
+class Nums a where
+  add :: a -> a -> a
+  sub :: a -> a -> a
+  scale :: Double -> a -> a
+  neg :: a -> a
+
+
 newtype Vector
   = Vector [Double]
 
 newtype Matrix
   = Matrix [Vector]
+
