e191338d056f68094286b136190c3ef51843d456
thg19 <thg19@ic.ac.uk>
2020-06-30 15:54:26 +0100

    implement add for Vector

--- src/Math.hs
+++ src/Math.hs
@@ -8,6 +8,19 @@ class Nums a where
 newtype Vector
   = Vector [Double]
 
+instance Nums Vector where
+  add (Vector xs) (Vector ys)
+    = Vector $ add' xs ys
+      where
+        add' :: [Double] -> [Double] -> [Double]
+        add' [] ys
+          = ys
+        add' xs []
+          = xs
+        add' (x:xs) (y:ys)
+          = (x + y) : add' xs ys
+
+
 newtype Matrix
   = Matrix [Vector]
 
